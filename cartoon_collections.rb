6e98ece5141d92aed8a41dd369433c87963f699c
Derek phung <dragevards@gmail.com>
2020-03-31 18:25:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types)
-    return cheese_types.first
+    return 
   else
     nil
   end
